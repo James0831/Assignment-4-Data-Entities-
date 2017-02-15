e058a5116181d639533d633c5d8c21bcba5d343f
James0831 <jdcarroll08@gmail.com>
2017-02-15 10:15:41 -0800

    Add files via upload

---
+++ Register.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        SubmitRegistration();
+    }
+    protected void SubmitRegistration()
+    {
+        string lastname = LastNameTextBox.Text;
+        string firstname = FirstNameTextBox.Text;
+        string email = EmailTextBox.Text;
+        string apartment = ApartmentTextBox.Text;
+        string street = StreetTextBox.Text;
+        string city = CityTextBox.Text;
+        string state = StateTextBox.Text;
+        string zipcode = ZipcodeTextBox.Text;
+        string home = HomePhoneTextBox.Text;
+        string work = WorkPhoneTextBox.Text;
+        string password = PasswordTextBox.Text;
+
+        Community_AssistEntities cae = new Community_AssistEntities();
+        int result = cae.usp_Register(lastname, firstname, email, password,
+            apartment, street, city, state, zipcode, home, work);
+        if (result != -1)
+        {
+            Response.Redirect("Default.aspx");
+        }
+        else
+        {
+            ResultLabel.Text = "Something went wrong!";
+        }
+    }
+}
